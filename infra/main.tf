62b750c5fd57a9236333948ec7a2da896aa8a962
Raj Vaya <raj.vaya2017@gmail.com>
2024-09-24 17:30:49 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"  # Specify the desired version
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
 provider "docker" {}
 
 resource "docker_image" "backend" {
